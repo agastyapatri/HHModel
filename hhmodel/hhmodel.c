ec263a0902d0ae01ee6f69626529b0bb9915ebee
agastyapatri <patriagastya5@gmail.com>
2023-07-10 19:08:25 +0530

    project restructuring

---
+++ hhmodel/hhmodel.c
@@ -0,0 +1,10 @@
+#define HHMODEL_H
+#ifndef HHMODEL_H
+    #include "hhmodel.h"
+#endif 
+
+#include <stdio.h>
+#include <math.h>
+float testfunction(int x){
+    return sin(x);
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+/********************************************************************
+ * Simulating the Hodkgin Huxley Model of Neuronal Dynamics
+ * Written by Agastya Patri
+********************************************************************/
+#include <stdio.h>
+int main(){
+    printf("%d\n", 10000);
+}
